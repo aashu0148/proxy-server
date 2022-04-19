9a526e1b9ab054a9188faeb7714d91c6a46e4b49
unknown <buildforfb@gmail.com>
2022-04-19 23:05:29 +0530

    inital

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/proxy", async (req, res) => {
+  const url = req.query.url;
+  if (!url) {
+    res.status(422).json({
+      message: "No url found.",
+    });
+    return;
+  }
+  const response = await axios.get(url);
+
+  res.status(200).json({
+    data: response.data,
+  });
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Backend is up at : ${process.env.PORT || 5000}`);
+});
